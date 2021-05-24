9e4b523ec5fc6b46a8cb0a94af7d4cb8b6a83081
Connor Fleming <piracyonmars@gmail.com>
2021-05-24 17:52:30 +0100

    Build.sh uses && rather than ;

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-rgbasm -o main.o main.asm;
-rgblink -o metronome.gb main.o;
-rgbfix -v -p 0 metronome.gb;
-rm *.o
+rgbasm -o main.o main.asm && rgblink -o metronome.gb main.o && rgbfix -v -p 0 metronome.gb && rm *.o
